fb44e1d874be34c87c6c4dc13bb903ba40bb44f8
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-03-24 14:29:46 +0530

    first commit

---
+++ src/main/java/com/springboot/lang4j/SpringbootLang4jApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.lang4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLang4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLang4jApplication.class, args);
+	}
+
+}
